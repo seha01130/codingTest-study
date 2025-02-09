84945d8aa49167228333888600a321b978953e70
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-02-09 16:56:00 +0900

    [Bronze II] Title: 주사위 네개, Time: 136 ms, Memory: 16324 KB -BaekjoonHub

--- 백준/Bronze/2484. 주사위 네개/주사위 네개.java
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
+        int answer = 0; //가장 많은 상금을 받은 사람의 상금을 저장할 변수
+        int num = Integer.parseInt(br.readLine()); //참여하는 사람의 수
 
-        String[] strings;
-        int answer = 0;
+        String[] strings; //주사위를 던진 4개의 눈이 저장될 배열
+
         for(int i = 0; i < num; i++){
             strings = br.readLine().split(" ");
             
-            HashMap<Integer, Integer> counts = new HashMap<>();
-            int count = 0;
+            HashMap<Integer, Integer> counts = new HashMap<>(); //key = 주사위의 눈, value = 해당 주사위의 눈이 나온 갯수
+            int count = 0; //주사위의 눈이 나온 갯수를 세기 위한 변수
             for (int j = 1; j <= 6; j++){
                 for (String s : strings){
-                    if (j == Integer.parseInt(s)){
+                    if (j == Integer.parseInt(s)){ //현재 주사위의 눈과 던져진 눈이 같을 경우 count++
                         count++;
                     }
                 }
@@ -34,32 +33,29 @@ public class Main {
             // System.out.println(counts);
             
             int diceNum = 0;
-            int DoubleTwoNum = 0;
+            int DoubleTwoNum = 0; 
             int maxPrice = 0;
             int price = 0;
 
             Set<Map.Entry<Integer, Integer>> entrySet = counts.entrySet();
             Iterator<Map.Entry<Integer, Integer>> itr = entrySet.iterator();
-            Integer twoDouble[] = new Integer[2];
+            Integer twoDouble[] = new Integer[2]; //같은 눈이 2개씩 1쌍 혹은 2쌍 나올 경우 이 배열에 담는다
             while(itr.hasNext()){
                 Map.Entry<Integer, Integer> eachEntry = itr.next();
-                // System.out.println("밸류값 : " + eachEntry.getValue());
+                // System.out.println("value값 : " + eachEntry.getValue());
                 if (eachEntry.getValue() == 4){
                     diceNum = eachEntry.getKey();
                     price = 50000 + diceNum * 5000;
-                    // break;
                 } else if (eachEntry.getValue() == 3){
                     diceNum = eachEntry.getKey();
                     price = 10000 + diceNum * 1000;
-                    // break;
                 } else if (eachEntry.getValue() == 2){
                     twoDouble[DoubleTwoNum] = eachEntry.getKey();
                     
-                    if (DoubleTwoNum == 0) {
+                    if (DoubleTwoNum == 0) { //같은 눈이 2개씩 1쌍일때
                         price = 1000 + eachEntry.getKey() * 100;
-                    } else {
+                    } else {                //같은 눈이 2개씩 2쌍일때
                         price = 2000 + twoDouble[0] * 500 + twoDouble[1] * 500;
-                        // break;
                     }
 
                     DoubleTwoNum++;
