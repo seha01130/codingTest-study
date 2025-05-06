6867a1d074149cb5bc978dec775d52639ec2d500
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-05-06 17:19:26 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -4,25 +4,31 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
 
-        long digitSum = 0;
-        for (int i = 1; i <= N; i++){
-            digitSum += getDigits(i);
+        // x자리수 // 1 2 3 ... 이렇게  ++ 하면서 증가해야함
+        int length = 1;
+
+        // 1 10 100 1000 이렇게 10씩 곱하면서 커짐
+        int start = 1;
+
+        long result = 0;
+
+        while (start * 10 - 1 <= N){ 
+            result += length * (start * 9); //자리수갯수 * 해당자리수에 존재하는 숫자갯수
+            length++;
+            start *= 10;
         }
 
-        sb.append(digitSum);
-        System.out.println(sb);
-    }
+        result += length * (N - start + 1);
 
-    public static int getDigits(int num){
-        int count = 0;
-        while (num != 0){
-            count++;
-            num /= 10;
-        }
-        return count;
+        System.out.println(result);
     }
 }
+
+
+// 1 ~ 9 : 9개
+// 10 ~ 99 : 90개
+// 100 ~ 999 : 900개
+
+// 102 -> 100 ~ 102 -> 3개 -> 102 - 100 + 1
