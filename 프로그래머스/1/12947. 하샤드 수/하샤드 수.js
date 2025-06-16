bc76cca344e28a888793356a1476a248112fe187
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-17 00:14:06 +0900

    [level 1] Title: 하샤드 수, Time: 3.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -2,5 +2,5 @@ function solution(x) {
     let sumOfDigits = [...x.toString()].reduce((acc, cur) => acc + Number(cur), 0);
     console.log(sumOfDigits);
     
-    return (x % sumOfDigits) === 0 ? true : false;
+    return !(x % sumOfDigits);
 }
