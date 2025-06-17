94fff9d204289b6065dade8f7b0c2f7f3c3fe51e
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-17 19:06:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,3 @@
 function solution(absolutes, signs) {
-    let sum = 0;
-    for (let i = 0; i < absolutes.length; i++){
-        if (signs[i] === true){
-            sum += absolutes[i];
-        } else {
-            sum -= absolutes[i];
-        }
-    }
-    return sum;
+    return absolutes.reduce((acc, cur, i) => acc + cur * (signs[i] ? 1 : -1) , 0);
 }
