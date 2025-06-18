a11a785795c5317aa24111234c79f6d7efb59174
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-18 22:54:36 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,11 +1,5 @@
 function solution(s) {
     let len = s.length;
-    let index;
-    if (len % 2 === 0){
-        index = len / 2 - 1;
-        return s[index] + s[index + 1];
-    } else {
-        index = Math.floor(len / 2);
-        return s[index];
-    }
+    let startIdx = Math.floor(len/2);
+    return len % 2 === 0 ? s.slice(startIdx-1, startIdx+1) : s.slice(startIdx, startIdx+1);
 }
