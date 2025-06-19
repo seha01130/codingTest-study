3e5361ccfc9e484f58bb70f45f5b104f9b826b10
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-19 15:39:40 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.38 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -3,18 +3,26 @@ function solution(arr) {
         return [-1];
     }
     
-    let minIdx = arr.reduce((minIndex, cur, index, arr) => {
-        return cur < arr[minIndex] ? index : minIndex;
-    }, 0);
-    console.log(minIdx);
+//     1.    
+//     let minIdx = arr.reduce((minIndex, cur, index, arr) => {
+//         return cur < arr[minIndex] ? index : minIndex;
+//     }, 0);
+//     console.log(minIdx);
     
-    arr.splice(minIdx, 1);
+//     arr.splice(minIdx, 1);
+//     return arr;
     
+//     2. 
 //     let min = Math.min(...arr);
 //     let minIndex = arr.indexOf(min);
 //     console.log(minIndex);
     
 //     arr.splice(minIndex, 1);
+//     return arr;
+ 
+//     3. 
+    let min = Math.min(...arr);
+    let answer = arr.filter(e => e !== min);
     
-    return arr;
+    return answer;
 }
