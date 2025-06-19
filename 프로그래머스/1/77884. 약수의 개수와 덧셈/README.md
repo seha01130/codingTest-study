a6d44b03a75ed9629c3ed8fd39759bebe96e022f
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-19 20:43:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.3 MB, 시간: 3.42 ms
+메모리: 33.5 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 20:01:49
+2025년 06월 19일 20:43:49
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,21 +1,7 @@
 function solution(left, right) {
     let result = 0;
     for(let i = left; i <= right; i++){
-        numOfDiv(i) === true ? result += i : result -= i;
+        Number.isInteger(Math.sqrt(i)) === true ? result -= i : result += i;
     }
     return result;
-}
-
-function numOfDiv(n) {
-    let count = 1;
-    for (let i = 2; i <= n ; i++) {
-        if (n % i === 0){
-            count++;
-        }
-    }
-    if (count % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
 }
