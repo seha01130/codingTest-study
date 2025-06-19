d9efeaaf90d640b5e12562dc21bc2436c55e843c
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-19 15:57:40 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-    let result = 0;
-    for (let i = 0; i < a.length; i++){
-        result += a[i] * b[i];
-    }
-    
-    return result;
+    return a.reduce((acc, _, i) => acc + a[i] * b[i], 0);
 }
