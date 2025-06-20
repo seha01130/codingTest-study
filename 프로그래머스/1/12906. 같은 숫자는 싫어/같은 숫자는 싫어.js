454bfd0349f49ec464007f41849f8dd7ee788d7f
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-20 15:05:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 50.60 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,11 +2,13 @@ function solution(arr)
 {
     var answer = [];
     
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] !== arr[i - 1]){
-            answer.push(arr[i]);
-        }
-    }
+    // for (let i = 0; i < arr.length; i++){
+    //     if (arr[i] !== arr[i - 1]){
+    //         answer.push(arr[i]);
+    //     }
+    // }
         
-    return answer;
+    // return answer;
+    
+    return arr.filter((e, i) => e !== arr[i - 1]);
 }
