b050697d7e8062fec714da8f19ea71185441baae
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-06-21 12:31:57 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,15 +1,28 @@
 function solution(d, budget) {
-    let answer = 0;
+//     let answer = 0;
     
-    d.sort((a,b) => a - b);
-    for (let i = 0; i < d.length; i++){
-        budget -= d[i];
+//     d.sort((a,b) => a - b);
+//     for (let i = 0; i < d.length; i++){
+//         budget -= d[i];
         
-       if (budget < 0){
-           break;
-       } 
-        answer++;
-    }
+//        if (budget < 0){
+//            break;
+//        } 
+//         answer++;
+//     }
     
-    return answer;
+//     return answer;
+    
+    // return d.sort((a,b) => a - b).reduce((acc, cur) => {
+    //     if (budget - cur >= 0){
+    //         budget -= cur;
+    //         return acc + 1;
+    //     } else {
+    //         return acc;
+    //     }
+    // }, 0);
+    
+    return d.sort((a,b) => a - b).reduce((acc, cur) => {
+        return (budget -= cur) >= 0 ? acc+1 : acc;
+    }, 0)
 }
