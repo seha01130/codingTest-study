a7c820de31d86499e6811cd2a4dda8ff0e23c584
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2025-10-17 23:28:39 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 672 ms, Memory: 37988 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
