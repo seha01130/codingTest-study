f3387cfd91ac17cd1c1f10a917c05ee6d3c4edaf
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2026-03-04 15:14:45 +0900

    [Silver II] Title: DOM, Time: 368 ms, Memory: 53796 KB -BaekjoonHub

--- 백준/Silver/10552. DOM/DOM.java
+++ 백준/Silver/10552. DOM/DOM.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
@@ -21,41 +23,44 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] graph = new ArrayList[M+1];
+        List<Integer>[] graph = new ArrayList[M + 1];
         for(int i = 1; i <= M; i++){
             graph[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int like = Integer.parseInt(st.nextToken());
-            int hate = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            graph[hate].add(like);
+            graph[b].add(a);
         }
 
-        // 정리 완료
+        // 간선 저장 끝
 
         boolean[] visited = new boolean[M+1];
         int count = 0;
-        int currentChannel = P;
-        while(true){
-            if (visited[currentChannel]){
+
+        while (true) {
+            if (visited[P] == true){
                 count = -1;
                 break;
             }
-
-            if (graph[currentChannel].size() == 0){
+            if (graph[P].size() == 0){
                 break;
             }
 
-            visited[currentChannel] = true;
-            
-            currentChannel = graph[currentChannel].get(0);
+            visited[P] = true;
             count++;
+            P = graph[P].get(0);
         }
-        
-        System.out.println(count);
+
+
+        sb.append(count);
+        System.out.println(sb);
     }
 }
 
+/*
+
+*/
