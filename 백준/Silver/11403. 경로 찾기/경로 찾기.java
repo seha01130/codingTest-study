de3a4e5d03e3084e7635009522ef1cdd28f8854b
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2026-03-04 17:48:08 +0900

    [Silver I] Title: 경로 찾기, Time: 164 ms, Memory: 16616 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,58 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-
+        int N = Integer.parseInt(br.readLine());
         int[][] graph = new int[N][N];
 
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
-                int num = Integer.parseInt(st.nextToken());
-                graph[i][j] = num;
+        // 1. 입력: 인접 행렬을 그대로 배열에 저장
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // System.out.println(Arrays.deepToString(graph));
-
-        // 간선 저장 끝
-
-        // i에서 j까지 가는 길을 탐색할거야
-        // 근데 k에서 한 번 멈춰서 경로를 나눌거야
-        for(int k = 0; k < N; k++){
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    if (graph[i][j] == 1){
-                        continue;
-                    }  
-                    
-                    if (graph[i][k] == 1 && graph[k][j] == 1){
+        // 2. 플로이드-워셜 알고리즘 (3중 for문)
+        // k: 거쳐가는 노드 (반드시 가장 바깥 루프여야 함!)
+        for (int k = 0; k < N; k++) {
+            // i: 출발 노드
+            for (int i = 0; i < N; i++) {
+                // j: 도착 노드
+                for (int j = 0; j < N; j++) {
+                    // i에서 k로 갈 수 있고, k에서 j로 갈 수 있다면?
+                    // i에서 j로 갈 수 있다는 뜻! (1로 업데이트)
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
                         graph[i][j] = 1;
                     }
                 }
             }
         }
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
+        // 3. 출력: 업데이트된 행렬 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 sb.append(graph[i][j]).append(" ");
             }
             sb.append("\n");
         }
-
+        
         System.out.println(sb);
     }
 }
