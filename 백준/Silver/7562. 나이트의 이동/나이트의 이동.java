d0f54540881aa72f38e3c3c7c58793cee7e98238
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2026-03-21 22:57:05 +0900

    [Silver I] Title: 나이트의 이동, Time: 280 ms, Memory: 80776 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0){
+            int I = Integer.parseInt(br.readLine());
+            int[][] graph = new int[I][I];
+            boolean[][] visited = new boolean[I][I];
+            
+            String[] current = br.readLine().split(" ");
+            String[] target = br.readLine().split(" ");
+
+            int curR = Integer.parseInt(current[0]);
+            int curC = Integer.parseInt(current[1]);
+            int targetR = Integer.parseInt(target[0]);
+            int targetC = Integer.parseInt(target[1]);
+
+            int[] dr = {-2, -1, 1, 2, 2, 1, -1, -2};
+            int[] dc = {1, 2, 2, 1, -1, -2, -2, -1};
+
+            sb.append(bfs(graph, visited, curR, curC, targetR, targetC, dr, dc, I)).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+
+    private static int bfs(int[][] graph, boolean[][] visited, int curR, int curC, int targetR, int targetC, int[] dr, int[]dc, int I) {
+        Queue<int[]> queue = new LinkedList<>();
+        int count = 0;
+
+        queue.offer(new int[] {curR, curC, count});
+        visited[curR][curC] = true;
+
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int cr = node[0];
+            int cc = node[1];
+            count = node[2];
+
+            if (cr == targetR && cc == targetC){
+                return count;
+            }
+
+            for (int i = 0; i < 8; i++){
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+
+                if (nr < 0 || nc < 0 || nr > I-1 || nc > I-1 || visited[nr][nc] == true) continue;
+
+                queue.offer(new int[] {nr, nc, count + 1});
+                visited[nr][nc] = true;
+            }
+        }
+
+        return 0;
+    }
+}
+
+/*
+bfs - 최단거리
+
+나이트가 한 번에 움직일 수 있는 거리는 (r, c)으로 보면
+(-2, 1) (-1, 2) (1, 2) (2, 1) (2, -1) (1, -2) (-1, -2) (-2, -1) 이다
+
+저 순서대로 돌면서 bfs로 목표지점 최단거리 찾기
+*/
