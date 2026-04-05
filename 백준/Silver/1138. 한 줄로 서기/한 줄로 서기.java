6546df7a0d91fb7d086c68c155a8c8733c6a19e4
suyeon Yoon <102642679+seha01130@users.noreply.github.com>
2026-04-05 17:08:13 +0900

    [Silver II] Title: 한 줄로 서기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -20,40 +20,37 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] inputArr = new int[N+1];
-        int[] answerArr = new int[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++){
             inputArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++){
+        ArrayList<Integer> answerList = new ArrayList<>();
+
+        for (int i = N; i >= 1; i--){
             int tallerNum = inputArr[i];
-            int count = 0;
-            for (int j = 1; j <= N; j++){
-                if (answerArr[j] != 0) continue;
-                if (tallerNum == count){
-                    answerArr[j] = i;
-                }
-                count++;
-            }
+            answerList.add(tallerNum, i);
         }
 
-        for (int i = 1; i <= N; i++) {
-            sb.append(answerArr[i]).append(" ");
+        for (int i = 0; i < N; i++){
+            sb.append(answerList.get(i)).append(" ");
         }
-
         
         System.out.println(sb);
     }
 }
 
 /*
-1번 방법
+1번째 방법
 n개의 배열 마련해놓고
 키 작은사람부터 확인하면서 자기보다 큰 사람의 수만큼 빈 공간 두고 그 다음칸에 넣기.
 앞으로 확인할 사람은 나보다 큰 사람이니까 그 사람이 들어갈 공간을 앞에 둬놓는거야.
 
-2번 방법
-
+2번째 방법
+ArrayList로 중간에 끼어들 수 있게 정답 받음
+키 큰 사람부터 확인하면서 자기보다 큰 사람의 수를 index로 하여 ArrayList에 끼워넣기 함.
+-> 큰 사람부터 확인하니까 어차피 다음에 나올 사람은 나보다 작은 사람임. 그럼 나보다 큰 사람의 수에 영향을 주는 사람이 이후에 없을거라는 뜻.
+-> 따라서 자기보다 큰 사람의 수를 index로 하면 나중에 내 앞에 끼어드는 사람이 있다고 해도 여전히 나보다 큰 사람의 수는 동일함.
+(ArrayList 자료구조를 쓴 이유임! 중간에 인덱스가 끼어들어도 그 뒤는 알아서 자연스럽게 뒤로 밀리니까!)
 */
